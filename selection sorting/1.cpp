ab9197f534ce8cce59fa2f84488c9add706cde55
Adarsh <ar6893@srmist.edu.in>
2024-01-22 21:05:59 +0530

    edited the code

--- selection sorting/1.cpp
+++ selection sorting/1.cpp
@@ -6,7 +6,7 @@ int main() {
 
     int arr[5] = {5,4,3,2,1};
 
-    for(int i = 0; i<5; i++) {
+    for(int i = 0; i<5-1; i++) {
         for(int j = i+1; j<5; j++) {
             if(arr[i] > arr[j]) {
                 swap(arr[i],arr[j]);
