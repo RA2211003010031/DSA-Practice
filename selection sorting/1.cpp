97cbebc96613d30fc47bd784d4c3656f4dbdf9ab
Adarsh <ar6893@srmist.edu.in>
2024-01-22 11:36:13 +0530

    Selection sorting code initialized using array

---
+++ selection sorting/1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int arr[5] = {5,4,3,2,1};
+
+    for(int i = 0; i<5; i++) {
+        for(int j = i+1; j<5; j++) {
+            if(arr[i] > arr[j]) {
+                swap(arr[i],arr[j]);
+            }
+        }
+    }
+
+    cout<<"After sorting:"<<endl;
+    for(int i =0 ; i<5; i++) {
+        cout<<arr[i]<<" ";
+    }
+ 
+    return 0;
+}
