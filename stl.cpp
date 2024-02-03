526836b0ac6c370650b9da188b838bf4a28c9fac
Adarsh <ar6893@srmist.edu.in>
2024-02-03 23:50:13 +0530

    stl - inital commit

---
+++ stl.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    // algorithms - containers - functions - iterators
+
+    pair<int,int> p = {2,3};
+    cout<<p.first<<endl<<p.second<<endl;
+
+    return 0;
+}
